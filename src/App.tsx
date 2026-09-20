67f3c1a4722cf2c147418bc15deae6b0b9a77c22
Koksik <dododon111111@gmail.com>
2026-09-20 22:03:31 +0300

    da

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,23 @@ function App(){
         <div className={styles.bigText}> Emoji Finder</div>
         <div className={styles.smallText}>Find emoji by keywords</div>
       </div>
-      
     </div>
+
     <div className={styles.inpText}>
       <input type="text" placeholder='Placeholder'/>
     </div>
+
     <div className={styles.emojis}>
 
+
       <div className={styles.card}>
-        <div className={styles.icon}>💯</div>
-        <div className={styles.emName}>100</div>
-        <div className={styles.emDiscr}>Hundred, points, symbol, wow, win, perfect, parties</div>  
+          <div className={styles.inCard}>
+            <div className={styles.icon}>💯</div>
+            <div className={styles.emName}>100</div>
+            <div className={styles.emDiscr}>Hundred, points, symbol, wow, win, perfect, parties</div>  
+         </div>
       </div>
+
 
       <div className={styles.card}>
         <div className={styles.inCard}>
@@ -27,14 +32,16 @@ function App(){
             <div className={styles.emName}>1234</div>
             <div className={styles.emDiscr}>input symbol for numbers symbol</div>
         </div>
-
       </div>  
 
+
       <div className={styles.card}>
-        <div className={styles.icon}>🔢</div>
-        <div className={styles.emName}>1234</div>
-        <div className={styles.emDiscr}>input symbol for numbers symbol</div>  
-      </div>
+        <div className={styles.inCard}>
+            <div className={styles.icon}>🔢</div>
+            <div className={styles.emName}>1234</div>
+            <div className={styles.emDiscr}>input symbol for numbers symbol</div>
+        </div>
+      </div>  
 
     </div>
     </>
